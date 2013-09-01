79bec7e5abb09fe30fd56ac47c533c43d6922383
Wyatt Carss <wyatt@freshbooks.com>
2013-09-01 16:55:07 -0400

    Modify template to support data.

--- app.py
+++ app.py
@@ -15,13 +15,26 @@ def datetime_encoder(o):
     else:
         raise TypeError(repr(o) + " is not JSON serializable")
 
+@app.route('/js')
+def json_orgs():
+    return jsonify({
+        'organizations': build_page_data(config.organization_names)
+    })
+
 @app.route('/')
-def pull_requests():
-    return jsonify(build_page_data(config.organization_names))
-#    return render_template(
-#        'index.html',
-#        orgs=build_page_data(config.organization_names)
-#    )
+def fake_index():
+    with open("fake_orgs.js") as fake_org_file:
+        fake_orgs = json.load(fake_org_file)
+    return render_template(
+        'index.html',
+        organizations=fake_orgs['organizations'],
+    )
+
+def index():
+    return render_template(
+        'index.html',
+        orgs=build_page_data(config.organization_names)
+    )
 
 def build_page_data(organization_names):
     orgs = []
@@ -40,14 +53,19 @@ def get_org_urls(names):
 def get_organization(org_url):
     full_organization = api_get(org_url)
     return {
-        'name': full_organization['name'],
+        'name': full_organization['login'],
+        'description': full_organization['name'],
         'api_url': full_organization['url'],
         'html_url': full_organization['html_url'],
         'repos': get_repos(full_organization['url'] + '/repos')
     }
 
 def api_get(resource):
-    return json.loads(requests.get(resource).text)
+    response = requests.get(resource)
+    print "Requested: %s" % resource
+    print "Response headers: %s" % response.headers
+    return response.json()
+    #return json.loads(requests.get(resource).text)
 
 def get_repos(repos_url):
     repos = []
@@ -58,7 +76,7 @@ def get_repos(repos_url):
     return repos
 
 def make_minimal_repo(full_repo):
-    pull_requests = get_pull_requests(full_repo['url'] + '/pulls/open')
+    pull_requests = get_pull_requests(full_repo['url'] + '/pulls?state=open')
     return {
         'name': full_repo['name'],
         'description': full_repo['description'],
@@ -78,31 +96,39 @@ def get_pull_requests(pull_url):
     return pulls
 
 def make_minimal_pull(full_pull):
-    comments = get_comments(full_pull['_links']['comments']['href'])
+    print full_pull
+    comments = get_comments(full_pull['_links']['review_comments']['href'])
     return {
         'number': full_pull['number'],
         'name': full_pull['title'],
         'description': full_pull['body'],
-        'state': full_pull['open'],
+        'state': full_pull['state'],
         'created_at': full_pull['created_at'],
         'updated_at': full_pull['updated_at'],
-        'api_url': full_pull['api_url'],
+        'api_url': full_pull['url'],
         'html_url': full_pull['html_url'],
         'comments': comments,
         'comment_count': len(comments),
     }
 
 def get_comments(comments_url):
+    comments = []
     full_comments = api_get(comments_url)
+    for full_comment in full_comments:
+        comments.append(make_minimal_comment(full_comment))
+    return comments
+
+def make_minimal_comment(full_comment):
+    print "full comment: %s" % full_comment
     return {
-        'body': full_comments['body'],
-        'id': full_comments['id'],
-        'api_url': full_comments['url'],
-        'html_url': full_comments['_links']['html']['href'],
-        'user': full_comments['user'],
-        'created_at': full_comments['created_at'],
-        'updated_at': full_comments['updated_at'],
-        'position': full_comments['position'],
+        'body': full_comment['body'],
+        'id': full_comment['id'],
+        'api_url': full_comment['url'],
+        'html_url': full_comment['_links']['html']['href'],
+        'user': full_comment['user'],
+        'created_at': full_comment['created_at'],
+        'updated_at': full_comment['updated_at'],
+        'position': full_comment['position'],
     }
 
 if __name__ == '__main__':
