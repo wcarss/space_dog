cb1a43912c40e917bdd98d8dbc417880c0f200b5
Wyatt Carss <wyatt@freshbooks.com>
2013-09-03 21:12:18 -0400

    Another nearly total rewrite.

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
 import json
 import requests
-from flask import Flask, render_template, jsonify
 import config
+from flask import Flask, render_template, jsonify
+import datetime
 
 app = Flask(__name__)
+count = 0
 
 def datetime_encoder(o):
-    from datetime import date
-    from datetime import datetime
-    if isinstance(o, datetime):
+    if isinstance(o, datetime.datetime):
         return o.isoformat()
-    elif isinstance(o, date):
+    elif isinstance(o, datetime.date):
         return o.isoformat()
     else:
         raise TypeError(repr(o) + " is not JSON serializable")
@@ -18,7 +18,8 @@ def datetime_encoder(o):
 @app.route('/js')
 def json_orgs():
     return jsonify({
-        'organizations': build_page_data(config.organization_names)
+        'organizations': build_page_data(config.organization_names),
+        'fetched_at': datetime.datetime.now().isoformat()
     })
 
 @app.route('/')
@@ -28,6 +29,7 @@ def fake_index():
     return render_template(
         'index.html',
         organizations=fake_orgs['organizations'],
+        fetched_at=fake_orgs['fetched_at']
     )
 
 def index():
@@ -38,17 +40,10 @@ def index():
 
 def build_page_data(organization_names):
     orgs = []
-    org_urls = get_org_urls(organization_names)
-    for org_url in org_urls:
-        organization = get_organization(org_url)
+    for name in organization_names:
+        organization = get_organization(config.api_url + '/orgs/' + name)
         orgs.append(organization)
     return orgs
-
-def get_org_urls(names):
-    urls = []
-    for name in names:
-        urls.append(config.api_url + '/orgs/' + name)
-    return urls
 
 def get_organization(org_url):
     full_organization = api_get(org_url)
@@ -57,13 +52,16 @@ def get_organization(org_url):
         'description': full_organization['name'],
         'api_url': full_organization['url'],
         'html_url': full_organization['html_url'],
-        'repos': get_repos(full_organization['url'] + '/repos')
+        'repos': get_repos(full_organization['url'] + '/repos?per_page=100')
     }
 
 def api_get(resource):
+    global count
+    count += 1
+    print "Made %s calls." % count
     response = requests.get(resource)
-    print "Requested: %s" % resource
-    print "Response headers: %s" % response.headers
+    #print "Requested: %s" % resource
+    #print "Response headers: %s" % response.headers
     return response.json()
     #return json.loads(requests.get(resource).text)
 
@@ -76,7 +74,7 @@ def get_repos(repos_url):
     return repos
 
 def make_minimal_repo(full_repo):
-    pull_requests = get_pull_requests(full_repo['url'] + '/pulls?state=open')
+    pull_requests = get_pull_requests(full_repo['url'] + '/pulls?state=open?per_page=100')
     return {
         'name': full_repo['name'],
         'description': full_repo['description'],
@@ -93,11 +91,16 @@ def get_pull_requests(pull_url):
         pull = make_minimal_pull(full_pull)
         pulls.append(pull)
 
+    pulls.reverse()
     return pulls
 
 def make_minimal_pull(full_pull):
-    print full_pull
+    #print full_pull
     comments = get_comments(full_pull['_links']['review_comments']['href'])
+    avatar_url = full_pull['user']['avatar_url']
+    default_avatar_url = "https://secure.gravatar.com/avatar/d41d8cd98f00b204e9800998ecf8427e?d=https://github.2ndsiteinc.com%2Fimages%2Fgravatars%2Fgravatar-user-420.png"
+    if avatar_url == default_avatar_url:
+        avatar_url = avatar_url.replace(".2ndsiteinc", "")
     return {
         'number': full_pull['number'],
         'name': full_pull['title'],
@@ -105,31 +108,33 @@ def make_minimal_pull(full_pull):
         'state': full_pull['state'],
         'created_at': full_pull['created_at'],
         'updated_at': full_pull['updated_at'],
+        'user': full_pull['user']['login'],
+        'avatar_url': avatar_url,
         'api_url': full_pull['url'],
         'html_url': full_pull['html_url'],
-        'comments': comments,
+        #'comments': comments,
         'comment_count': len(comments),
     }
 
 def get_comments(comments_url):
     comments = []
-    full_comments = api_get(comments_url)
-    for full_comment in full_comments:
-        comments.append(make_minimal_comment(full_comment))
-    return comments
+    full_comments = api_get(comments_url + '?per_page=100')
+    #for full_comment in full_comments:
+    #    comments.append(make_minimal_comment(full_comment))
+    return full_comments
 
 def make_minimal_comment(full_comment):
-    print "full comment: %s" % full_comment
+    #print "full comment: %s" % full_comment
     return {
         'body': full_comment['body'],
         'id': full_comment['id'],
         'api_url': full_comment['url'],
         'html_url': full_comment['_links']['html']['href'],
-        'user': full_comment['user'],
+        'user': full_comment['user']['login'],
         'created_at': full_comment['created_at'],
         'updated_at': full_comment['updated_at'],
         'position': full_comment['position'],
     }
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0')
